4f1180b1987b46c46ff4cd193fb5b0e33dc58cf6
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-11-29 19:34:17 -0800

    Update interactions to include payload

--- slack/listeners/interactions.js
+++ slack/listeners/interactions.js
@@ -1,6 +1,7 @@
 const displayTeamCreate = require('../controllers/displayTeamCreate');
 const createTeam = require('../controllers/createTeam');
 const addUserToTeam = require('../controllers/addUserToTeam');
+const displayTeam = require('../controllers/displayTeam');
 
 const Team = require('../../models/Team');
 
@@ -12,10 +13,21 @@ module.exports = (slackInteractions, web) => {
   });
 
   // Handles the user's submission of the "team create" modal
-  slackInteractions.action({ view: { callbackId: 'submit_team'} }, (payload, response) => createTeam(payload, Team));
-  // slackInteractions.action({ view: { callbackId: 'submit_team'} }, (payload, response) => {
-  //   console.log(payload);
-  // });
+  // slackInteractions.action({ view: { callbackId: 'submit_team'} }, (payload, response) => createTeam(payload, Team));
+  slackInteractions.action({ view: { callbackId: 'submit_team'} }, (payload, response) => {
+    // console.log('response', response);
+    const message = {
+      channel: 'iesd-bot',
+      text: 'Hello world',
+      // blocks: Team(teams)
+    }
+    try{
+      displayTeam(web, Team);
+    }
+    catch(err) {
+      console.log('No work', err);
+    }
+  });
 
   // Handles adding the current user when they click "choose" to the team
   slackInteractions.action({ actionId: 'team_select' }, (payload, response) => addUserToTeam(payload, Team));
