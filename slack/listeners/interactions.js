18405fadd35bf8506a6363f7d03fc16e129b9790
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-12-03 23:01:05 -0800

    Remove unused comment and code from interactions.js

--- slack/listeners/interactions.js
+++ slack/listeners/interactions.js
@@ -1,44 +1,18 @@
 const displayTeamCreate = require('../controllers/displayTeamCreate');
 const createTeam = require('../controllers/createTeam');
-const addUserToTeam = require('../controllers/addUserToTeam');
-const displayTeam = require('../controllers/displayTeam');
 const handleSelectTeam = require('../controllers/handleSelectTeam');
-const generateTeamView = require('../controllers/generateTeamBlock');
 
 const Team = require('../../models/Team');
 
 module.exports = (slackInteractions, web) => {
 
-  // slackInteractions.action({ actionId: 'team_select' }, (payload, response) => {
-  //   console.log('payload');
-  //   // web.im.open({
-  //   //   user: 'UERTLGB9C'
-  //   // });
-  // });
   // Handles adding the current user when they click "choose" to the team
   slackInteractions.action({ actionId: 'team_select' }, (payload, response) => handleSelectTeam(web, payload, Team));
-    // slackInteractions.action({ actionId: 'team_select' }, async (payload, response) => {
-    //   addUserToTeam(payload, Team);
-    //   const teamBlock = await generateTeamView(Team);
-    //   web.chat.update({
-    //     channel: payload.channel.id,
-    //     ts: payload.message.ts,
-    //     blocks: teamBlock
-    //   });
-    // });
 
   // Handles displaying the "team create" modal
   slackInteractions.action({ actionId: 'create_team' }, (payload, response) => displayTeamCreate(web, payload.trigger_id));
 
   // Handles the user's submission of the "team create" modal
   slackInteractions.action({ view: { callbackId: 'submit_team'} }, (payload, response) => createTeam(payload, Team));
-
-
-  // slackInteractions.action({ actionId: 'team_select' }, (payload, response) => handleSelectTeam(payload, Team));
-
-
-  // slackInteractions.action({ actionId: 'team_select' }, (payload, response) => {
-  //   console.log(payload);
-  // });
 
 }
