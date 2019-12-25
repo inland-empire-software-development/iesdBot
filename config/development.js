c8bdd39f672b1d0c9cfdbff476b03067248604c2
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-12-24 18:15:29 -0800

    Configure Docker to add seed data to database

--- config/development.js
+++ config/development.js
@@ -1,3 +1,12 @@
-module.exports = {
-  mongoURI: 'mongodb://localhost/IESDSlackTest',
+console.log(process.env.DOCKER);
+console.log(process.env.DOCKER === 'DOCKER');
+
+if(process.env.DOCKER === 'DOCKER'){
+  module.exports = {
+    mongoURI: 'mongodb://mongo:27017'
+  }
+} else {
+  module.exports = {
+    mongoURI: 'mongodb://localhost/IESDSlackTest'
+  }
 }
---
+++ mongo/seed/users.js
@@ -0,0 +1,6 @@
+db.IESDSlackTest.insert({
+  teamMembers: ["UFL950KSP"],
+  teamName: "test",
+  teamSetting: "open",
+  dateOfEvent: new Date("2019-10-30T14:51:14.626+00:00")
+});
