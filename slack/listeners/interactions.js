e646453d2725dbe728e0e959b42131e030780c72
tonynguyen111997 <tonynguyen111997@gmail.com>
2020-01-08 00:06:51 -0800

    Add initial code for handling edit_team_info view submission

--- slack/listeners/interactions.js
+++ slack/listeners/interactions.js
@@ -21,6 +21,39 @@ module.exports = (slackInteractions, web) => {
   slackInteractions.action({ actionId: 'create_team' }, (payload, response) => displayTeamCreate(web, payload.trigger_id));
 
   // Handles the user's submission of the "team create" modal
-  slackInteractions.viewSubmission({ callbackId: 'submit_team' }, (payload) => createTeam(payload, Team))
+  slackInteractions.viewSubmission({ callbackId: 'submit_team' }, (payload) => createTeam(payload, Team));
+
+  // Handles the user's submission of the "edit team info" modal
+  slackInteractions.viewSubmission({ callbackId: 'edit_team_info' }, async (payload) => {
+    const values = payload.view.state.values;
+
+    let teamName;
+    const teamOwner = payload.user.id;
+    let teamMembers;
+    let teamSetting;
+    let hackDayDate;
+  
+    for(let value in values){
+      if(values[value].hasOwnProperty('team_name')){
+        teamName = values[value].team_name.value;
+      } else if(values[value].hasOwnProperty('members')){
+        teamMembers = values[value].members.selected_users;
+        teamMembers.unshift(payload.user.id);
+      } else {
+        teamSetting = values[value].group_settings.selected_option.value;
+      }
+    }
+
+    console.log(teamName);
+    console.log(teamSetting);
+    console.log(teamMembers);
+
+    const test = await Team.find({ teamName })
+    console.log(test);
+    console.log('dang');
+    // Team.findOneAndUpdate({ teamName }, {
+    //   teamName: 'WOW'
+    // });
+  });
 
 }
