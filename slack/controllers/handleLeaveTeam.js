eb3006095ef3455785d987fbf00a00981ed5006c
tonynguyen111997 <tonynguyen111997@gmail.com>
2020-01-07 18:16:26 -0800

    Add closing modal after user clicks leave team in team manager

--- slack/controllers/handleLeaveTeam.js
+++ slack/controllers/handleLeaveTeam.js
@@ -1,15 +1,42 @@
+const client = require('../../lib/redis');
+
 const removeUserFromTeam = require('./removeUserFromTeam');
 const generateTeamBlock = require('./generateTeamBlock');
 const axios = require('axios');
 
-const handleLeaveTeam = async (payload, Team) => {
+const SectionText = require('../views/SectionText');
+
+const handleLeaveTeam = async (web, payload, Team) => {
   await removeUserFromTeam(payload, Team);
 
   const teamBlock = await generateTeamBlock(Team, payload.user.id);
 
-  axios.post(payload.response_url, {
+  const responseURL = await client.getResponseURL(payload.user.id);
+
+  axios.post(responseURL, {
     replace_original: 'true',
     blocks: teamBlock
+  })
+
+  web.views.update({
+    view_id: payload.view.id,
+    view: {
+      title: {
+        type: "plain_text",
+        text: "Edit Team Info",
+        emoji: true
+      },
+      type: "modal",
+      close: {
+        type: "plain_text",
+        text: "Close",
+        emoji: true
+      },
+      callback_id: "edit_team_info",
+      blocks: [
+        SectionText('You have successfully left the team.')
+      ]
+    }
   })
 }
 
