c8eafb376a4084b118c77c9ca1c187a6ab735b86
tonynguyen111997 <tonynguyen111997@gmail.com>
2020-01-08 15:29:51 -0800

    Separate code to update team info into 2 files, updateTeamInfo and handleEditTeamInfo

--- slack/controllers/displayTeamManager.js
+++ slack/controllers/displayTeamManager.js
@@ -6,14 +6,11 @@ const displayTeamManager = async (web, payload, db) => {
 
   client.setResponseURL(payload.user.id, payload.response_url);
 
-  const userTeam = await db.find({ teamMembers: payload.user.id });
-  const teamMembers = userTeam[0].teamMembers.filter((user) => {
-    return user !== payload.user.id;
-  });
+  const userTeam = await db.findOne({ teamMembers: payload.user.id });
 
   const modal = {
     trigger_id: payload.trigger_id,
-    view: TeamManagerModal(userTeam[0].teamName, teamMembers, userTeam[0].teamSetting)
+    view: TeamManagerModal(userTeam.teamName, userTeam.teamMembers, userTeam.teamSetting)
   }
   
   return web.views.open(modal);
--- slack/listeners/interactions.js
+++ slack/listeners/interactions.js
@@ -3,6 +3,7 @@ const createTeam = require('../controllers/createTeam');
 const handleSelectTeam = require('../controllers/handleSelectTeam');
 const handleLeaveTeam = require('../controllers/handleLeaveTeam');
 const displayTeamManager = require('../controllers/displayTeamManager');
+const handleEditTeamInfo = require('../controllers/handleEditTeamInfo');
 
 const Team = require('../../models/Team');
 
@@ -24,36 +25,6 @@ module.exports = (slackInteractions, web) => {
   slackInteractions.viewSubmission({ callbackId: 'submit_team' }, (payload) => createTeam(payload, Team));
 
   // Handles the user's submission of the "edit team info" modal
-  slackInteractions.viewSubmission({ callbackId: 'edit_team_info' }, async (payload) => {
-    const values = payload.view.state.values;
-
-    let teamName;
-    const teamOwner = payload.user.id;
-    let teamMembers;
-    let teamSetting;
-    let hackDayDate;
-  
-    for(let value in values){
-      if(values[value].hasOwnProperty('team_name')){
-        teamName = values[value].team_name.value;
-      } else if(values[value].hasOwnProperty('members')){
-        teamMembers = values[value].members.selected_users;
-        teamMembers.unshift(payload.user.id);
-      } else {
-        teamSetting = values[value].group_settings.selected_option.value;
-      }
-    }
-
-    console.log(teamName);
-    console.log(teamSetting);
-    console.log(teamMembers);
-
-    const test = await Team.find({ teamName })
-    console.log(test);
-    console.log('dang');
-    // Team.findOneAndUpdate({ teamName }, {
-    //   teamName: 'WOW'
-    // });
-  });
+  slackInteractions.viewSubmission({ callbackId: 'edit_team_info' }, (payload) => handleEditTeamInfo(payload, Team));
 
 }
