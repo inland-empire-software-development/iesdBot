3fecacbbb666ae3d1874051c88502f7413c1ca61
tonynguyen111997 <tonynguyen111997@gmail.com>
2020-01-16 10:23:43 -0800

    update removeUserFromTeam function to disband team if user is only one in team, and to update team owner if user is the current owner

--- slack/controllers/removeUserFromTeam.js
+++ slack/controllers/removeUserFromTeam.js
@@ -1,14 +1,32 @@
 const winston = require('../../config/winston');
 
-const removeUserFromTeam = async (payload, db) => {
-  db.updateOne({ teamMembers: payload.user.id }, { $pull: { teamMembers: payload.user.id }}, (err) => {
-    if(err){
-      winston.error(`Failed to remove ${payload.user.username}(${payload.user.id}) from team ${payload.actions[0].value}`);
-      return winston.error('Error:', err);
-    }
+const removeTeam = require('./removeTeam');
 
-    return winston.info(`Successfully removed ${payload.user.username}(${payload.user.id}) from team ${payload.actions[0].value}`);
-  })
+const removeUserFromTeam = async (payload, db) => {
+  const team = await db.findOne({ teamMembers: payload.user.id });
+
+  if(team.teamMembers.length <= 1){
+    removeTeam(payload, db);
+  } else if(team.teamOwner = payload.user.id){
+    db.updateOne({ teamMembers: payload.user.id }, { $pull: { teamMembers: payload.user.id }, teamOwner: team.teamMembers[1]}, (err) => {
+      if(err){
+        winston.error(`Failed to remove ${payload.user.username}(${payload.user.id}) from team ${payload.actions[0].value}`);
+        return winston.error('Error:', err);
+      }
+  
+      winston.info(`Successfully removed ${payload.user.username}(${payload.user.id}) from team ${payload.actions[0].value}`);
+      return winston.info(`The new owner of team ${team.teamName} is ${team.teamMembers[1]}.`);
+    })
+  } else {
+    db.updateOne({ teamMembers: payload.user.id }, { $pull: { teamMembers: payload.user.id }}, (err) => {
+      if(err){
+        winston.error(`Failed to remove ${payload.user.username}(${payload.user.id}) from team ${payload.actions[0].value}`);
+        return winston.error('Error:', err);
+      }
+  
+      return winston.info(`Successfully removed ${payload.user.username}(${payload.user.id}) from team ${payload.actions[0].value}`);
+    })
+  }
 }
 
 module.exports = removeUserFromTeam;
