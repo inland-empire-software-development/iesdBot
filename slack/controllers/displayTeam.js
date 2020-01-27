067811812f99af70b3ebe940d1a8b3134ac727aa
tonynguyen111997 <tonynguyen111997@gmail.com>
2020-01-26 21:21:51 -0800

    bot now DM user about teams

--- slack/controllers/displayTeam.js
+++ slack/controllers/displayTeam.js
@@ -4,13 +4,21 @@ const displayTeam = async (web, db, event) => {
 
   const teamBlock = await generateTeamBlock(db, event.user);
   
+  // const message = {
+  //   channel: 'iesd-bot',
+  //   user: event.user,
+  //   blocks: teamBlock
+  // }
+
+  // return web.chat.postEphemeral(message);
+
   const message = {
-    channel: 'iesd-bot',
-    user: event.user,
-    blocks: teamBlock
+    channel: event.user,
+    blocks: teamBlock,
+    as_user: true
   }
 
-  return web.chat.postEphemeral(message);
+  return web.chat.postMessage(message);
 }
 
 module.exports = displayTeam;
